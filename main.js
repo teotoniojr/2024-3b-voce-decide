799dbd219db6af890c2b3a7af8dce8de94466f37
Teotonio José de Souza Junior <102707827+teotoniojr@users.noreply.github.com>
2024-07-04 08:36:19 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -41,3 +41,22 @@ const perguntas = [
         ]
     },
 ]
+
+let atual = 0;
+let perguntaAtual;
+
+function mostrarPerguntas(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostrarAlternativas();
+}
+
+function mostrarAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mostrarPerguntas();
