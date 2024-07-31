021fa22776e6f8f94b530bd231ded442d840cf2d
Teotonio José de Souza Junior <102707827+teotoniojr@users.noreply.github.com>
2024-07-31 08:41:00 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -8,55 +8,105 @@ const perguntas = [
     {
         enunciado: "Enunciado 01",
         alternativas: [
-            "alternativa 01",
-            "alternativa 02"
+            {
+                texto:"alternativa 01",
+                afirmacao: "resultado 01"
+            },
+            {
+                texto:"alternativa 02",
+                afirmacao: "resultado 02"
+            }   
         ]
     },
     {
         enunciado: "Enunciado 02",
         alternativas: [
-            "alternativa 01",
-            "alternativa 02"
+            {
+                texto: "alternativa 03",
+                afirmacao: "resultado03"
+            },
+            {
+                texto: "alternativa 04",
+                afirmacao: "resultado 04"
+            }
         ]
     },
     {
         enunciado: "Enunciado 03",
         alternativas: [
-            "alternativa 01",
-            "alternativa 02"
+            {
+                texto: "alternativa 05",
+                afirmacao: "resultado 05"
+            },
+            {
+                texto: "alternativa 06",
+                afirmacao: "resultado 06"
+            }
         ]
     },
     {
         enunciado: "Enunciado 04",
         alternativas: [
-            "alternativa 01",
-            "alternativa 02"
+            {
+                texto: "alternativa 07",
+                afirmacao: "resultado 07"
+            },
+            {
+                texto: "alternativa 08",
+                afirmacao: "resultado 08"
+            }
         ]
     },
     {
         enunciado: "Enunciado 05",
         alternativas: [
-            "alternativa 01",
-            "alternativa 02"
+            {
+                texto: "alternativa 09",
+                afirmacao: "resultado 09"
+            },
+            {
+                texto: "alternativa 10",
+                afirmacao: "resultado 10"
+            }
         ]
     },
 ]
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostrarPerguntas(){
+    if(atual >= perguntas.length){
+        mostraResultado();
+        return
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = ""
     mostrarAlternativas();
 }
 
 function mostrarAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa))
         caixaAlternativas.appendChild(botaoAlternativas);
     }
+}
+
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostrarPerguntas();
+}
+
+function mostraResultado(){
+    caixaPerguntas.textContent = " Inicio do texto... "
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
 }
 
 mostrarPerguntas();
